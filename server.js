fbba2d92e3f93d9ede8151b7398cfdd5dc4b9093
Hung Chan <luuchanhunggs1@gmail.com>
2023-11-01 19:06:35 +0700

    fixed

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
-var http = require('http'),
-socketIO = require('socket.io'),
+server = require("http").Server();
+var SocketIO = require("socket.io");
+var io = SocketIO.listen(server);
 
-port = 8888,
-ip = '127.0.0.1',
-
-server = http.createServer().listen(port, ip, function(){
-    console.log('Socket.IO server started at %s:%s!', ip, port);
-}),
-
-io = socketIO.listen(server);
-
-var run = function(socket){
-    console.log('connect....');
-
-
-    socket.on('echotest', function (data) {
-        console.log(data);
-    })
-
-    socket.on('message', function(msg){
-        console.log('Received: ' + msg);
-    });
-
-    socket.disconnect('disconnect', function() {
-        console.log('Client disconnect');
-    });
-}
-
-io.sockets.on('connection', socket => {{
-    console.log('connect....');
-    socket.emit('hello', 'Hello from Socket.IO');
-    socket.send('Hello from SocketIO');
-
-}});
+io.sockets.on("connection", function (socket) {
+  console.log("connect");
+  socket.on("message", function (data) {
+    console.log(data);
+  });
+});
+server.listen(3000);
